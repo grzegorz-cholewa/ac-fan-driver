4f1b081921104b1eae2b152bffd319398e7f5761
grzegorz-cholewa <grzesiekcholewa@outlook.com>
2019-07-07 19:09:00 +0200

    Sorted functions

--- main.c
+++ main.c
@@ -66,106 +66,17 @@ uint32_t gate_pulse_delay_counter_us = 0;
 uint32_t pid_pulse_delay_counter_us = 0;
 
 /* FUNCTION PROTOTYPES */
-void gpio_init(void);
-void led_blink(uint8_t count, uint32_t on_off_cycle_period_ms);
-void interrupt_init(void);
-void timer_start(uint32_t time_us);
-uint32_t get_gate_delay_us(fan_gate_t * fan);
-void set_gate_state(fan_gate_t * fan, gate_state_t pulse_state);
-void update_input_data(void);
 void drive_fan(fan_gate_t * fan);
+uint32_t get_gate_delay_us(fan_gate_t * fan);
+void gpio_init(void);
+void interrupt_init(void);
+void led_blink(uint8_t count, uint32_t on_off_cycle_period_ms);
 void pid_regulator(fan_gate_t * fan, sensors_t * sensor_values);
+void set_gate_state(fan_gate_t * fan, gate_state_t pulse_state);
+void timer_start(uint32_t time_us);
+void update_input_data(void);
 
 /* FUNCTION DEFINITIONS */
-void gpio_init(void)
-{
-	ioport_configure_pin(LED_PIN, IOPORT_DIR_OUTPUT |  IOPORT_INIT_LOW);
-	ioport_configure_pin(GPIO_PUSH_BUTTON_0, IOPORT_DIR_INPUT | IOPORT_PULL_UP);
-	ioport_configure_pin(ZERO_CROSSING_PIN, IOPORT_DIR_INPUT | IOPORT_PULL_UP);
-	ioport_configure_pin(FAN1_DRIVE_PIN, IOPORT_DIR_OUTPUT | IOPORT_INIT_HIGH);
-	ioport_configure_pin(FAN2_DRIVE_PIN, IOPORT_DIR_OUTPUT | IOPORT_INIT_HIGH);	
-}
-
-void led_blink(uint8_t blink_count, uint32_t on_off_cycle_period_ms)
-{
-	while (blink_count > 0)
-	{
-		LED_On(LED_PIN);
-		delay_ms(on_off_cycle_period_ms/2);
-		LED_Off(LED_PIN);
-		delay_ms(on_off_cycle_period_ms/2);
-		blink_count--;
-	}
-}
-
-void interrupt_init(void)
-{
-	    EICRA |= (1 << ISC01 || 1 << ISC00);    // set INT0 to trigger on rising edge
-	    EIMSK |= (1 << INT0);     // activates INT0
-	    sei();                    // turn on interrupts
-}
-
-void timer_start(uint32_t time_us)
-{
-    uint32_t prescaler_value = 8;
-	uint32_t second_us = 1000000;
-	uint8_t value = time_us*(clock_speed/prescaler_value)/second_us-1; // for 100us timer value is 199
-		
-    OCR1A = value; 
-    TCCR1B |= (1 << WGM12); // Mode 4, CTC on OCR1A
-    TIMSK1 |= (1 << OCIE1A); // set interrupt on compare match
-    TCCR1B |= (1 << CS11); // set prescaler
-    sei(); // enable interrupts
-}
-
-uint32_t get_gate_delay_us(fan_gate_t * fan)
-{
-	double activation_angle_rad = acos(fan->mean_voltage/230.0); // acos function input is double, value from -1 to 1
-	uint32_t gate_delay = HALF_SINE_PERIOD_US*activation_angle_rad/(PI/2.0);
-	
-	if (gate_delay > MAX_GATE_DELAY_US)
-		return MAX_GATE_DELAY_US - ZERO_CROSSING_OFFSET_US;
-	if (gate_delay < MIN_GATE_DELAY_US)
-		return MIN_GATE_DELAY_US - ZERO_CROSSING_OFFSET_US;
-	
-	//return 8000 - ZERO_CROSSING_OFFSET_US; // const delay, for debugging
-	return gate_delay - ZERO_CROSSING_OFFSET_US;
-}
-
-void set_gate_state(fan_gate_t * fan, gate_state_t state)
-{
-	if (fan->state == state)
-	{
-		return; // no state change
-	}
-	
-	fan->state = state;
-	if (state == GATE_ACTIVE)
-	{
-		if (fan->index == fan1.index)
-			gpio_set_pin_low(FAN1_DRIVE_PIN); // optotransistor is active low
-		if (fan->index == fan2.index)
-			gpio_set_pin_low(FAN2_DRIVE_PIN);
-	}
-	
-	if (state != GATE_ACTIVE)
-	{
-		if (fan->index == fan1.index)
-			gpio_set_pin_high(FAN1_DRIVE_PIN);
-		if (fan->index == fan2.index)
-			gpio_set_pin_high(FAN2_DRIVE_PIN);
-	}
-}
-
-void update_input_data(void)
-{
-	read_temperatures(&sensor_values);
-	pid_regulator(&fan1, &sensor_values);
-	pid_regulator(&fan2, &sensor_values);
-	fan1.activation_delay_us = get_gate_delay_us(&fan1);
-	fan2.activation_delay_us = get_gate_delay_us(&fan2);
-}
-
 void drive_fan(fan_gate_t * fan)
 {
 	switch (work_state)
@@ -177,13 +88,13 @@ void drive_fan(fan_gate_t * fan)
 				set_gate_state(fan, GATE_IDLE);
 				break;
 			}
-				
+			
 			if (fan->mean_voltage>=MAX_FAN_VOLTAGE)
 			{
 				set_gate_state(fan, GATE_ACTIVE);
 				break;
 			}
-				
+			
 			if ( (gate_pulse_delay_counter_us >= fan->activation_delay_us) && (gate_pulse_delay_counter_us <= (fan->activation_delay_us + GATE_PULSE_TIME_US)) )
 			{
 				
@@ -202,6 +113,48 @@ void drive_fan(fan_gate_t * fan)
 		case WORK_STATE_FORCE_OFF:
 		set_gate_state(fan, GATE_IDLE);
 		break;
+	}
+}
+
+uint32_t get_gate_delay_us(fan_gate_t * fan)
+{
+	double activation_angle_rad = acos(fan->mean_voltage/230.0); // acos function input is double, value from -1 to 1
+	uint32_t gate_delay = HALF_SINE_PERIOD_US*activation_angle_rad/(PI/2.0);
+	
+	if (gate_delay > MAX_GATE_DELAY_US)
+	return MAX_GATE_DELAY_US - ZERO_CROSSING_OFFSET_US;
+	if (gate_delay < MIN_GATE_DELAY_US)
+	return MIN_GATE_DELAY_US - ZERO_CROSSING_OFFSET_US;
+	
+	//return 8000 - ZERO_CROSSING_OFFSET_US; // const delay, for debugging
+	return gate_delay - ZERO_CROSSING_OFFSET_US;
+}
+
+void gpio_init(void)
+{
+	ioport_configure_pin(LED_PIN, IOPORT_DIR_OUTPUT |  IOPORT_INIT_LOW);
+	ioport_configure_pin(GPIO_PUSH_BUTTON_0, IOPORT_DIR_INPUT | IOPORT_PULL_UP);
+	ioport_configure_pin(ZERO_CROSSING_PIN, IOPORT_DIR_INPUT | IOPORT_PULL_UP);
+	ioport_configure_pin(FAN1_DRIVE_PIN, IOPORT_DIR_OUTPUT | IOPORT_INIT_HIGH);
+	ioport_configure_pin(FAN2_DRIVE_PIN, IOPORT_DIR_OUTPUT | IOPORT_INIT_HIGH);	
+}
+
+void interrupt_init(void)
+{
+	EICRA |= (1 << ISC01 || 1 << ISC00);    // set INT0 to trigger on rising edge
+	EIMSK |= (1 << INT0);     // activates INT0
+	sei();                    // turn on interrupts
+}
+
+void led_blink(uint8_t blink_count, uint32_t on_off_cycle_period_ms)
+{
+	while (blink_count > 0)
+	{
+		LED_On(LED_PIN);
+		delay_ms(on_off_cycle_period_ms/2);
+		LED_Off(LED_PIN);
+		delay_ms(on_off_cycle_period_ms/2);
+		blink_count--;
 	}
 }
 
@@ -228,7 +181,7 @@ void pid_regulator(fan_gate_t * fan, sensors_t * sensor_values)
 	integral = integral + error;
 	
 	#ifdef PROPORTIONAL_OUTPUT_REGULATION // PROPORTIONAL REGULATION, FOR TESTING ONLY
-   
+	
 	mean_voltage = 115 - 5*error;
 	//mean_voltage = current_temp *3;
 	// mean_voltage = 75; // for setting const value
@@ -240,13 +193,60 @@ void pid_regulator(fan_gate_t * fan, sensors_t * sensor_values)
 	#endif
 	
 	if (mean_voltage >= MAX_FAN_VOLTAGE)
-		mean_voltage = FAN_FULL_ON_VOLTAGE;
+	mean_voltage = FAN_FULL_ON_VOLTAGE;
 	
 	if (mean_voltage <= MIN_FAN_VOLTAGE)
-		mean_voltage = FAN_OFF_VOLTAGE;
+	mean_voltage = FAN_OFF_VOLTAGE;
 	
 	fan->mean_voltage = mean_voltage;
 };
+
+void set_gate_state(fan_gate_t * fan, gate_state_t state)
+{
+	if (fan->state == state)
+	{
+		return; // no state change
+	}
+	
+	fan->state = state;
+	if (state == GATE_ACTIVE)
+	{
+		if (fan->index == fan1.index)
+		gpio_set_pin_low(FAN1_DRIVE_PIN); // optotransistor is active low
+		if (fan->index == fan2.index)
+		gpio_set_pin_low(FAN2_DRIVE_PIN);
+	}
+	
+	if (state != GATE_ACTIVE)
+	{
+		if (fan->index == fan1.index)
+		gpio_set_pin_high(FAN1_DRIVE_PIN);
+		if (fan->index == fan2.index)
+		gpio_set_pin_high(FAN2_DRIVE_PIN);
+	}
+}
+
+void timer_start(uint32_t time_us)
+{
+	uint32_t prescaler_value = 8;
+	uint32_t second_us = 1000000;
+	uint8_t value = time_us*(clock_speed/prescaler_value)/second_us-1; // for 100us timer value is 199
+	
+	OCR1A = value;
+	TCCR1B |= (1 << WGM12); // Mode 4, CTC on OCR1A
+	TIMSK1 |= (1 << OCIE1A); // set interrupt on compare match
+	TCCR1B |= (1 << CS11); // set prescaler
+	sei(); // enable interrupts
+}
+
+void update_input_data(void)
+{
+	read_temperatures(&sensor_values);
+	pid_regulator(&fan1, &sensor_values);
+	pid_regulator(&fan2, &sensor_values);
+	fan1.activation_delay_us = get_gate_delay_us(&fan1);
+	fan2.activation_delay_us = get_gate_delay_us(&fan2);
+}
 
 
 int main (void)
